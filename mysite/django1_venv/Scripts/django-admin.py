fed15587a3772adbe8c601d40715b1be40bafb37
CyborgVillager <almawijonathan@gmail.com>
2020-03-21 09:19:00 -0500

    v0 Django-01 Respo Created
    Django-01 Respo Created: Respo created, Django-01 Project
    
                                Files Changed:
                                * Installation of Django & its components
                                * django1_venv
                                * README & Version_Info files
    
                                Objectives:
                                * Make an site reference/example
                                    - Index/main page, which will hold
                                        -> Nav to other tab pages
                                        -> Basic Lorem Info, Info of site introduction
                                            -Images
                                            - Separate Sections
                                            - About Lorem Lorem
                                        -> Footer
    
                                    * Nav tab Post -> Lorem Posts
                                        - Nav Bar
                                        - example posts by the user/admin
                                        - Footer
    
                                    * Nav tab Contact -> Contact Lorem
                                        - Nav Bar
                                        - Contact lorem example
                                        - Footer
    
                                    * CSS/JS Implement
                                        - Styles for the site
                                        - Lorem, etc to connect to pages
    
                                    * Footer
                                        - Lorem copyright
                                        - Lorem email
                                        - Lorem social media logo's

---
+++ mysite/django1_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\PyProject\GitRespo\Web Dev\Full Stack\Django1\Django-01\mysite\django1_venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
